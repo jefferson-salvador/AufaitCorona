f2509cfbc10d29bf70dead911908aec1ba237920
Mark Up <markup.scrum@gmail.com>
2020-11-27 13:16:39 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+var input = document.getElementById("search");
+input.addEventListener("keyup", function(event) {
+  if (event.keyCode === 13) {
+   event.preventDefault();
+   changeCountry();
+  }
+});
+
+fetch(`https://corona.lmao.ninja/v2/countries/Philippines`)
+.then((response)=>{
+    return response.json();
+})
+.then((data)=>{
+    document.getElementById("country").innerHTML = data.country;
+    document.getElementById("flag").src = data.countryInfo.flag;
+    document.getElementById("active").innerHTML = data.active.toLocaleString();
+})
+
+const changeCountry = () =>{
+    var country = document.getElementById("search").value;
+    fetch(`https://corona.lmao.ninja/v2/countries/${country}`)
+    .then((response)=>{
+        return response.json();
+    })
+    .then((data)=>{
+        document.getElementById("country").innerHTML = data.country.toLocaleString();
+        document.getElementById("active").innerHTML = data.active.toLocaleString();
+    })
+}
