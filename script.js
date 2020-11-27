fc3ab1894e3ba21a423e42eb2b35b6eb2e35fa1d
Mark Up <markup.scrum@gmail.com>
2020-11-27 14:10:52 +0800

    updated code

--- script.js
+++ script.js
@@ -16,6 +16,9 @@ fetch(`https://corona.lmao.ninja/v2/countries/Philippines`)
     document.getElementById("active").innerHTML = data.active.toLocaleString();
     document.getElementById("critical").innerHTML = data.critical.toLocaleString();
     document.getElementById("recovered").innerHTML = data.recovered.toLocaleString();
+    document.getElementById("cases").innerHTML = data.cases.toLocaleString();
+    document.getElementById("deaths").innerHTML = data.deaths.toLocaleString();
+    document.getElementById("tests").innerHTML = data.tests.toLocaleString();
 })
 
 const changeCountry = () =>{
@@ -29,5 +32,9 @@ const changeCountry = () =>{
         document.getElementById("flag").src = data.countryInfo.flag;
         document.getElementById("active").innerHTML = data.active.toLocaleString();
         document.getElementById("critical").innerHTML = data.critical.toLocaleString();
+        document.getElementById("cases").innerHTML = data.cases.toLocaleString();
+        document.getElementById("deaths").innerHTML = data.deaths.toLocaleString();
+        document.getElementById("tests").innerHTML = data.tests.toLocaleString();
     })
+
 }
